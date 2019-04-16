ec3a608cd21a5ee9aeee25ad7e1c73949beb0075
Mike Jolley <mike.jolley@me.com>
2019-04-16 12:53:05 +0100

    Enable selecting with tab key on selectWoo

--- src/js/select2/core.js
+++ src/js/select2/core.js
@@ -327,12 +327,11 @@ define([
     $(document).on('keydown', function (evt) {
       var key = evt.which;
       if (self.isOpen()) {
-        if (key === KEYS.ESC || key === KEYS.TAB ||
-            (key === KEYS.UP && evt.altKey)) {
+        if (key === KEYS.ESC || (key === KEYS.UP && evt.altKey)) {
           self.close();
 
           evt.preventDefault();
-        } else if (key === KEYS.ENTER) {
+        } else if (key === KEYS.ENTER || key === KEYS.TAB) {
           self.trigger('results:select', {});
 
           evt.preventDefault();
